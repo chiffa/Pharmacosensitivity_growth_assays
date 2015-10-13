c2106f840643d4e17dec60bc3685f4fbfbf2c2ab
chiffa <ank@andreikucharavy.com>
2015-10-13 18:37:14 -0400

    Added code to properly perform log-transform and generate the fitness plot we are interested in.

--- src/post_processing.py
+++ src/post_processing.py
@@ -6,6 +6,7 @@ import matplotlib as mp
 import plot_drawings as PD
 from pickle import load
 from chiffatools.Linalg_routines import gini_coeff
+from chiffatools.dataviz import smooth_histogram
 
 memdict = load(open('../analysis_runs/memdict.dmp', 'r'))
 #[drug, cell_line] -> (means, mean errs, unique_concs), (mean_arr, err_arr, unique, T0)
@@ -45,12 +46,16 @@ if __name__ == '__main__':
         _75 = np.nanpercentile(means_pad, 75, axis=0)
         _50 = np.nanpercentile(means_pad, 50, axis=0)
         _25 = np.nanpercentile(means_pad, 25, axis=0)
-        selector = np.logical_and(_75 < .8, _25 > 0.1 )
+        selector = np.logical_and(_25 < .9, _75 > 0.1 )
         if any(selector):
-            sel2 = np.argmin(np.sum(np.isnan(means_pad[:, selector]), axis=0))
-            concs_effective_range.append((drug, cell_lines, unique_c[selector][sel2],
-                                          means_pad[:, selector][:, sel2][:, np.newaxis],
-                                          errs_pad[:, selector][:, sel2][:, np.newaxis]))
+            concs_effective_range.append((drug, cell_lines, unique_c[selector],
+                                          means_pad[:, selector],
+                                          errs_pad[:, selector]))
+
+            # sel2 = np.argmin(np.sum(np.isnan(means_pad[:, selector]), axis=0))
+            # concs_effective_range.append((drug, cell_lines, unique_c[selector][sel2],
+            #                               means_pad[:, selector][:, sel2][:, np.newaxis],
+            #                               errs_pad[:, selector][:, sel2][:, np.newaxis]))
 
     # method 2
     all_cell_lines_arr = np.array(list(all_cell_lines))
@@ -60,10 +65,14 @@ if __name__ == '__main__':
     subtitle_accumulator = []
 
     for elt in concs_effective_range:
-        names_accumulator.append(elt[0]+" - %.2E"%elt[2])
+        # names_accumulator.append(elt[0]+" - %.2E"%elt[2])
+        names_accumulator += [elt[0]+" - %.2E"%conc for conc in elt[2]]
         cell_lines = elt[1]
         names_pad = np.array(list(all_cell_lines.difference(set(cell_lines))))
         names_array = np.hstack((np.array(cell_lines), names_pad))
+
+        print elt[3].shape
+
         means = np.pad(elt[3], ((0, names_pad.shape[0]), (0, 0)),
                        mode='constant', constant_values=((np.nan, np.nan), (np.nan, np.nan)))
         errs = np.pad(elt[4], ((0, names_pad.shape[0]), (0, 0)),
@@ -84,36 +93,145 @@ if __name__ == '__main__':
     means_accumulator = means_accumulator.tolist()
     errs_accumulator = errs_accumulator.tolist()
     all_cell_lines = all_cell_lines.tolist()
-    ramp = np.linspace(0, len(names_accumulator), len(names_accumulator)).tolist()
+
 
     idx = all_cell_lines.index('BT483')
-    argsorter = np.argsort(np.array(means_accumulator[idx]))
+    # idx = all_cell_lines.index('HCC1143')
+    # idx = all_cell_lines.index('HCC1569')
+    # idx = all_cell_lines.index('ZR751')
+    # idx = all_cell_lines.index('ZR75B')
+    idx1 = all_cell_lines.index('184A1')
+    idx2 = all_cell_lines.index('184B5')
+
+    mean_for_proxy_WT = np.nanmean(np.array(means_accumulator)[[idx1, idx2], :], axis=0)
+    errs_for_proxy_WT = np.nanmean(np.array(errs_accumulator)[[idx1, idx2],:], axis=0)
+
+    all_cell_lines.append('WT_proxy')
+    means_accumulator.append(mean_for_proxy_WT.tolist())
+    errs_accumulator.append(errs_for_proxy_WT.tolist())
+
+    # support = np.logical_and(
+    #                 np.logical_not(np.isnan(means_accumulator[idx])),
+    #                 np.logical_or(
+    #                     np.logical_not(np.isnan(means_accumulator[idx1])),
+    #                     np.logical_not(np.isnan(means_accumulator[idx2]))))
+
+    # support = np.logical_or(
+    #                 np.logical_not(np.isnan(means_accumulator[idx1])),
+    #                 np.logical_not(np.isnan(means_accumulator[idx2])))
+
+    support = np.logical_not(np.isnan(means_accumulator[idx]))
+
+    average_stress_intesity = np.nanmean(np.array(means_accumulator)[:, support], axis=0)
+    log_std_stress_intensity = np.nanstd(np.log2(np.array(means_accumulator)[:, support]), axis=0)
+    # argsorter = np.argsort(mean_for_proxy_WT[support]/average_stress_intesity)
+    argsorter = np.argsort(average_stress_intesity)
+
+    average_stress_intesity = average_stress_intesity[argsorter]
+    log_std_stress_intensity = log_std_stress_intensity[argsorter]
+
+    ramp = np.linspace(0, argsorter.shape[0], argsorter.shape[0]).tolist()
     cmap = mp.cm.get_cmap(name='Paired')
+
+    gini_coeffs = []
+    mean_fitness = []
+
     for i, cell_line in enumerate(all_cell_lines):
-        g_coeff = gini_coeff(np.array(means_accumulator[i]))
-        support = np.sum(np.logical_not(np.isnan(np.array(means_accumulator[i]))))
-        if cell_line in ['184A1', '184B5']:
-            plt.errorbar(ramp, np.array(means_accumulator[i])[argsorter],
-                         yerr=np.array(errs_accumulator[i])[argsorter],
-                         fmt='*', label='%s - %.2f - %s'%(cell_line, g_coeff, support))
-        if cell_line == 'BT483':
-            plt.errorbar(ramp, np.array(means_accumulator[i])[argsorter],
-                         yerr=np.array(errs_accumulator[i])[argsorter],
-                         label='%s - %.2f - %s'%(cell_line, g_coeff, support),
-                         color='k')
-        elif support < 10:
+        means_array = np.array(means_accumulator[i])[support][argsorter]
+        errs_array = np.array(errs_accumulator[i])[support][argsorter]
+
+        g_coeff = gini_coeff(means_array)
+        support_size = np.sum(np.logical_not(np.isnan(np.array(means_accumulator[i]))))
+
+        if cell_line in ['184A1', '184B5'] or support_size < 10:
             continue
+
+        gini_coeffs.append(g_coeff)
+        mean_fitness.append(np.nanmean(means_array))
+        if i == idx:
+            plt.errorbar(ramp,
+                         np.log2(means_array),
+                         yerr=np.log2(1+errs_array),
+                         label='%s - %.2f - %s'%(cell_line, g_coeff, support_size),
+                         color='k')
+
         else:
-            plt.errorbar(ramp, np.array(means_accumulator[i])[argsorter],
-                         yerr=np.array(errs_accumulator[i])[argsorter],
-                         fmt='o', label='%s - %.2f - %s'%(cell_line, g_coeff, support),
+            plt.errorbar(ramp,
+                         np.log2(means_array),
+                         yerr=np.log2(1+errs_array),
+                         fmt='.', label='%s - %.2f - %s'%(cell_line, g_coeff, support_size),
                          color=cmap(i/float(len(names_accumulator))))
 
+    mean_g_coeff = gini_coeff(np.array(average_stress_intesity))
+    plt.plot(ramp,
+             np.log(average_stress_intesity),
+             color = 'r',
+             label = '%s - %.2f - %s'%('average', mean_g_coeff , argsorter.shape[0]))
+    plt.plot(ramp,
+             np.log(average_stress_intesity)+log_std_stress_intensity,
+             color = 'g')
+    plt.plot(ramp,
+             np.log(average_stress_intesity)-log_std_stress_intensity,
+             color = 'g')
+
     mp.rc('font', size=10)
-    plt.xticks(ramp, np.array(names_accumulator)[argsorter], rotation='vertical')
+    plt.xticks(ramp, np.array(names_accumulator)[support][argsorter], rotation='vertical')
     plt.subplots_adjust(bottom=0.25)
-    plt.legend(ncol=2)
+    # plt.legend(ncol=2)
     plt.show()
 
-    # TODO: it looks like the gini coefficient is strongly affected by the NaN values of experiments:
-    # we need to retain and compare only elements that have a similar support
+    # smooth_histogram(np.array(gini_coeffs))
+    # plt.show()
+
+    # smooth_histogram(np.array(mean_fitness))
+    # plt.show()
+
+    # plt.plot(np.array(gini_coeffs), np.array(mean_fitness), 'o')
+    # plt.xlabel('gini coefficient')
+    # plt.ylabel('average fitness across conditions')
+    # plt.show()
+
+    # method 4 and plotting
+    base_line = np.array(means_accumulator[idx])[support][argsorter]
+    accumulator = []
+
+    for i, cell_line in enumerate(all_cell_lines):
+
+        if cell_line == 'BT483' or support_size < 10:
+            continue
+
+        else:
+            log_transformed = np.log2(np.array(means_accumulator[i])[support][argsorter]/base_line)
+            accumulator.append(log_transformed)
+            plt.plot(ramp,
+                     log_transformed, '.',
+                     label='%s'%(cell_line),
+                     color=cmap(i/float(len(names_accumulator))))
+
+    # mp.rc('font', size=10)
+    # plt.xticks(ramp, np.array(names_accumulator)[support][argsorter], rotation='vertical')
+    # plt.subplots_adjust(bottom=0.25)
+    # plt.legend(ncol=2)
+    # plt.show()
+
+    accumulator = np.array(accumulator)
+
+    means = np.nanmean(accumulator, axis=0)
+    stds = np.nanstd(accumulator, axis=0)
+
+    # sorter = np.argsort(means)
+    # means = means[sorter]
+    # stds = stds[sorter]
+
+    plt.plot(ramp, means, label='mean')
+    plt.plot(ramp, stds, label = 'stds')
+    plt.xticks(ramp, np.array(names_accumulator)[support][argsorter], rotation='vertical')
+    # plt.legend()
+    plt.show()
+
+    plt.plot(means, stds, 'o')
+    plt.xlabel('means')
+    plt.ylabel('std')
+    plt.show()
+
+    # TODO: attention, we need STD of log2-othewise nothing will work properly
